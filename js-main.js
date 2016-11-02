f0a639e3a54d9a0a4a35cae4701334e6429e2f23
Zara Leventhal <zleventhal@gmail.com>
2016-11-01 21:31:37 -0400

    alpha first commit

---
+++ js-main.js
@@ -0,0 +1,21 @@
+// jwt custom code
+//
+// function pageSetUp() {
+// 	//console.log('pageSetUp');
+// 	document.getElementById("result").innerHTML = "00";
+//
+// 	var a=document.getElementsByTagName("a");
+// 	for(var i=0;i<a.length;i++)
+// 	{
+// 		 a[i].onclick=function()
+// 		 {
+// 			  window.location=this.getAttribute("href");
+// 			  return false
+// 		 }
+// 	}
+// }
+
+
+function jmp2LocalPage(whichPage) {
+	location.href = whichPage;
+}
